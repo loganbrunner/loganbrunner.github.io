643d54bf7f4cf74c82106c3fc6878f408e639196
Logan Brunner <logan.brunner@tno.nl>
2020-05-30 15:02:58 +0200

    Updates

---
+++ precache-manifest.e6a552735ac222bcc147efa1a1856353.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2d88ca78bb4919cd129ef6cdb9a671",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3ee63884de2314890af",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c7b30270b2d08c587ef",
+    "url": "/static/js/2.e5125459.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5125459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ee63884de2314890af",
+    "url": "/static/js/main.f3337816.chunk.js"
+  },
+  {
+    "revision": "852faae9980bbae83149",
+    "url": "/static/js/runtime-main.828858da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
